218403aa360579e68939a7520a8b340103d26a58
s4kr4 <s4kr4m4@gmail.com>
2019-12-20 15:10:44 +0900

    Add hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,53 @@
+local prefix = {"cmd", "ctrl"}
+hs.window.animationDuration = 0
+
+hs.hotkey.bind(prefix, "Up", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	f.x = max.x
+	f.y = max.y
+	f.w = max.w
+	f.h = max.h
+
+	win:setFrame(f)
+end)
+
+hs.hotkey.bind(prefix, "Left", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	f.x = max.x
+	f.y = max.y
+	f.w = max.w / 2
+	f.h = max.h
+
+	win:setFrame(f)
+end)
+
+hs.hotkey.bind(prefix, "Right", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	f.x = max.x + (max.w / 2)
+	f.y = max.y
+	f.w = max.w / 2
+	f.h = max.h
+
+	win:setFrame(f)
+end)
+
+hs.hotkey.bind("alt", "tab", function()
+	hs.window.switcher.nextWindow()
+end)
+
+hs.hotkey.bind({"alt", "shift"}, "tab", function()
+	hs.window.switcher.previousWindow()
+end)
+
