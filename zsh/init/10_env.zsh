29fabb62d9d07517f8a6a79906346a23d4ea3055
s4kr4 <s4kr4m4@gmail.com>
2025-08-26 11:04:22 +0900

    Fix PATH

--- zsh/init/10_env.zsh
+++ zsh/init/10_env.zsh
@@ -10,10 +10,6 @@ export LOCAL_ENV_PATH="${HOME}/.local/env.zsh"
 export AFX_BIN_DIR="${LOCAL_BIN_PATH}"
 export AFX_COMMAND_PATH="${LOCAL_BIN_PATH}"
 
-if is_osx; then
-    export PATH=$PATH:/usr/local/bin:/bin
-fi
-
 # Deploy local settings
 if [[ -e ${LOCAL_BIN_PATH} ]]; then
     mkdir -p ${LOCAL_BIN_PATH}
@@ -22,4 +18,6 @@ fi
 if [[ -e ${LOCAL_ENV_PATH} ]]; then
     source ${LOCAL_ENV_PATH}
 fi
+
+typeset -U PATH
 
