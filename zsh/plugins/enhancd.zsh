efc04b6d67e99b8eb6934ae3c2ef91ce74025a61
s4kr4 <s4kr4m4@gmail.com>
2026-01-25 21:03:46 +0900

    Update enhancd settings

--- zsh/plugins/enhancd.zsh
+++ zsh/plugins/enhancd.zsh
@@ -4,10 +4,6 @@ if [[ ! -f "$ENHANCD_DIR/init.sh" ]]; then
     return
 fi
 
-export ENHANCD_FILTER='
-    fzf --preview "eza -al --tree --level 1 --group-directories-first --git-ignore
-    --header --git --no-user --no-time --no-filesize --no-permissions {}"
-    --preview-window right,50% --height 35% --reverse --ansi
-'
+export ENHANCD_FILTER='fzf --preview "eza -al --tree --level 1 --group-directories-first --git-ignore --header --git --no-user --no-time --no-filesize --no-permissions {}" --preview-window right,50% --height 35% --reverse --ansi'
 
 source "$ENHANCD_DIR/init.sh"
